fd51ce983c2286927ce240217e67af60e9e785c5
madej <madejbart@gmail.com>
2024-11-09 11:30:21 +0100

    09.11 - new model for different endpoints 2

--- finance_app/analizer.py
+++ finance_app/analizer.py
@@ -4,7 +4,32 @@ import sqlite3
 from csv import DictWriter
 
 sql_select = """
-SELECT * FROM endpoint 
+SELECT endpoint_data_type FROM endpoint 
 WHERE enabled = 1 AND
 (date_updated IS NULL OR strftime('%s', 'now') - strftime('%s', date_updated) > frequency); 
 """
+base_url = "https://financialmodelingprep.com/api"
+data_type = 'historical-price-full/stock_dividend'
+
+class Analizer:
+    def __init__(self):
+        self.con = sqlite3.connect(r"C:\Users\madej\PycharmProjects\Finance_Project_Rev1\db.sqlite3")
+
+    def create_url(self, base_url, data_type):
+        self.base_url = base_url
+        self.data_type = data_type
+        cur = self.con.cursor()
+        res = cur.execute(sql_select)
+        print(res)
+        for endpoint_data_type in res:
+            print(endpoint_data_type)
+        self.final_url = .....
+
+    def run(self):
+
+
+
+
+if __name__ == "__main__":
+    myanalizer = Analizer()
+    myanalizer.create_url(base_url, data_type)
--- finance_app/models.py
+++ finance_app/models.py
@@ -49,7 +49,15 @@ class Result(models.Model):
 
 class Endpoint(models.Model):
     endpoint_name = models.CharField(max_length=20)
+    date_updated = models.DateTimeField(blank=True, null=True, default=datetime.now())
     endpoint_data_type = models.CharField(max_length=20)
     enabled = models.BooleanField(blank=True, null=True)
+    frequency = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'endpoints'
+        db_table = 'endpoint'
+
+
 
 
